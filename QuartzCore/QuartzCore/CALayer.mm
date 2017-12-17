86ca88949a6fa9410c855c770a3404d9705ed84e
AwayQuEM <awayquym@gmail.com>
2017-12-17 18:04:50 +0800

    CALayer & namespace CA
    Signed-off-by: AwayQuEM <awayquym@gmail.com>

--- QuartzCore/QuartzCore/CALayer.mm
+++ QuartzCore/QuartzCore/CALayer.mm
@@ -55,69 +55,60 @@ static BOOL _CAObject_automaticallyNotifiesObserversForKey(int arg0, int arg1) {
 
 
 - (void)layoutSublayers {
-
-    if ((*(int8_t *) (self->_attr.layer + 0x35) & 0x30) == 0x0) goto loc_ff4b9;
-
-    loc_ff425:
-    {
-
+    if ((*(int8_t *) (self->_attr.layer + 0x35) & 0x30) == 0x0) {
+        id caaction = [self actionForKey:@"onLayout"];
+        if (caaction != 0x0) {
+            [caaction runActionForKey:@"onLayout" object:self arguments:nil];
+        }
+        return;
+    }
 
     CA::Transaction *transaction = CA::Transaction::ensure();
     int32_t value0x20 = *transaction->value0x20;
-        *transaction->value0x20 += 1;
-        if (value0x20 == 0x0) {
-            os_unfair_lock_lock(transaction->value0x18);
-        }
-
-        if ((*(((CA::Layer *)(self->_attr.layer))->value0x34) & 0x10) != 0x0) {
-            goto loc_ff45e;
-        }
+    *transaction->value0x20 += 1;
+    if (value0x20 == 0x0) {
+        os_unfair_lock_lock(transaction->value0x18);
     }
 
+    if ((*(((CA::Layer *) (self->_attr.layer))->value0x34) & 0x10) != 0x0) {
+        SEL rcx = @selector(layoutSublayersOfLayer:);
+        if (rcx == 0x0) {
+
+            CA::Transaction::unlock();
+
+            id caaction = [self actionForKey:@"onLayout"];
+            if (caaction != 0x0) {
+                [caaction runActionForKey:@"onLayout" object:self arguments:nil];
+            }
+            return;
+        }
 
 
-    loc_ff450:
-    if ((*(((CA::Layer *)(self->_attr.layer))->value0x34) & 0x20) == 0x0) {
-        goto loc_ff4b1;
     }
-
-    loc_ff455:
+    if ((*(((CA::Layer *) (self->_attr.layer))->value0x34) & 0x20) == 0x0) {
+        CA::Transaction::unlock();
+        id caaction = [self actionForKey:@"onLayout"];
+        if (caaction != 0x0) {
+            [caaction runActionForKey:@"onLayout" object:self arguments:nil];
+        }
+        return;
+    }
     SEL rcx = @selector(_layoutSublayersOfLayer:);
-    goto loc_ff465;
-
-    loc_ff465:
-    {
-        r15 = *rcx;
-        if (sel == 0x0) goto loc_ff4b1;
-
+    if (rcx == 0x0) {
+        CA::Transaction::unlock();
+        id caaction = [self actionForKey:@"onLayout"];
+        if (caaction != 0x0) {
+            [caaction runActionForKey:@"onLayout" object:self arguments:nil];
+        }
+        return;
     }
 
-
-    loc_ff46d:
-    {
-
-    id layerDelegate = [((CA::Layer *)(self->_attr.layer))->value0x70 retain];
+    id layerDelegate = [((CA::Layer *) (self->_attr.layer))->value0x70 retain];
     CA::Transaction::unlock();
     [layerDelegate performSelector:r15 withObject:r14];
     [layerDelegate release];
     return;
-    }
 
-    loc_ff4b1:
-    CA::Transaction::unlock();
-    goto loc_ff4b9;
-
-    loc_ff4b9:
-    rax = [self actionForKey:@"onLayout"];
-    if (rax != 0x0) {
-        rcx = self;
-        [rax runActionForKey:@"onLayout" object:rcx arguments:0x0];
-    }
-    return;
-
-    loc_ff45e:
-    rcx = @selector(layoutSublayersOfLayer:);
-    goto loc_ff465;
 }
 
 - (void)layoutIfNeeded {
@@ -3111,8 +3102,7 @@ static BOOL _CAObject_automaticallyNotifiesObserversForKey(int arg0, int arg1) {
     rdx = [CATransition class];
     if ([r12 isKindOfClass:rdx] != 0x0) {
         r14 = @"transition";
-    }
-    else {
+    } else {
         rax = [r14 length];
         if (rax == 0x0) {
             r14 = rax;
@@ -3136,8 +3126,7 @@ static BOOL _CAObject_automaticallyNotifiesObserversForKey(int arg0, int arg1) {
     }
     if (CA::Transaction::get_value(*(r12 + 0x28), 0x18, 0x12) != 0x0) {
         xmm0 = intrinsic_movsd(xmm0, var_38);
-    }
-    else {
+    } else {
         xmm0 = intrinsic_movsd(xmm0, *0x17b5d8);
     }
     [r13 setDefaultDuration:rdx];
@@ -3205,19 +3194,19 @@ static BOOL _CAObject_automaticallyNotifiesObserversForKey(int arg0, int arg1) {
 //    r14 = *(r12 + *_OBJC_IVAR_$_CALayer._attr + 0x8);
 //    if ((*(int8_t *)(r14 + 0x35) & 0x1) == 0x0) goto loc_fa8b0;
     // TODO: exception
-    if ((*(((CA::Layer *)(self->_attr.layer))->value0x35) & 0x10) != 0x0) {
+    if ((*(((CA::Layer *) (self->_attr.layer))->value0x35) & 0x10) != 0x0) {
         goto loc_fa8b0;
     }
     loc_fa887:
 //    rdi = *(r14 + 0x70);
 //    if (rdi == 0x0) goto loc_fa8b0;
-    if ((((CA::Layer *)(self->_attr.layer))->value0x70) == nil) goto loc_fa8b0;
+    if ((((CA::Layer *) (self->_attr.layer))->value0x70) == nil) goto loc_fa8b0;
 
     loc_fa890:
     {
 
 //    rbx = [rdi actionForLayer:r12 forKey:var_38];
-    id caaction = [(((CA::Layer *)(self->_attr.layer))->value0x70) actionForLayer:self forKey:key];
+        id caaction = [(((CA::Layer *) (self->_attr.layer))->value0x70) actionForLayer:self forKey:key];
 //    if (rbx != 0x0) goto loc_faa13;
         if (caaction != 0x0) goto loc_faa13;
     }
@@ -3225,7 +3214,7 @@ static BOOL _CAObject_automaticallyNotifiesObserversForKey(int arg0, int arg1) {
     {
 //    rax = *(r14 + 0x80);
 //    var_30 = r15;
-    if ((((CA::Layer *)(self->_attr.layer))->value0x80 == 0x0) || (CA::AttrList::get(((CA::Layer *)(self->_attr.layer))->value0x80, 0x1, 0x1) == 0x0)) goto loc_fa8ff;
+        if ((((CA::Layer *) (self->_attr.layer))->value0x80 == 0x0) || (CA::AttrList::get(((CA::Layer *) (self->_attr.layer))->value0x80, 0x1, 0x1) == 0x0)) goto loc_fa8ff;
     }
 
     loc_fa8da:
@@ -3236,17 +3225,17 @@ static BOOL _CAObject_automaticallyNotifiesObserversForKey(int arg0, int arg1) {
     loc_fa8ff:
     var_48 = r12;
 //    if ((*(int8_t *)(r14 + 0x34) & 0x8) == 0x0) goto loc_fa9be;
-    if ((*(((CA::Layer *)(self->_attr.layer))->value0x34) & 0x8) == 0x0) goto loc_fa9be;
+    if ((*(((CA::Layer *) (self->_attr.layer))->value0x34) & 0x8) == 0x0) goto loc_fa9be;
 
     loc_fa90e:
 //    rax = *(r14 + 0x80);
 //    rbx = var_48;
 //    if ((rax == 0x0) || (CA::AttrList::get(*rax, 0x1cb, 0x1) == 0x0)) {
-    if (((((CA::Layer *)(self->_attr.layer))->value0x80 == 0x0) || (CA::AttrList::get((((CA::Layer *)(self->_attr.layer))->value0x80, 0x1cb, 0x1) == 0x0)) {
+    if (((((CA::Layer *) (self->_attr.layer))->value0x80 == 0x0) || (CA::AttrList::get((((CA::Layer *) (self->_attr.layer))->value0x80, 0x1cb, 0x1) == 0x0)) {
 //        rax = [rbx class];
         Class clazz = [self class];
 //        _CAObject_defaultValueForAtom(rax, 0x1cb, 0x1, var_40);
-        _CAObject_defaultValueForAtom((((CA::Layer *)(self->_attr.layer))->value0x80, 0x1cb, 0x1, var_40);
+        _CAObject_defaultValueForAtom((((CA::Layer *) (self->_attr.layer))->value0x80, 0x1cb, 0x1, var_40);
     }
     rax = var_40;
     if (rax == 0x0) goto loc_fa9be;
@@ -3277,8 +3266,7 @@ static BOOL _CAObject_automaticallyNotifiesObserversForKey(int arg0, int arg1) {
 //        if ((*(int32_t *)(*(r14 + *_OBJC_IVAR_$_CALayer._attr + 0x8) + 0x4) & 0x40) == 0x0) {
         if ((layer->b0x4 & 0x40) == 0x0) {
             rbx = 0x0;
-        }
-        else {
+        } else {
             rbx = [CATransaction _implicitAnimationForLayer:self keyPath:key];
         }
     }
@@ -3308,11 +3296,11 @@ static BOOL _CAObject_automaticallyNotifiesObserversForKey(int arg0, int arg1) {
 
     loc_fa56b:
     rax = *(rbx + *_OBJC_IVAR_$_CALayer._attr + 0x8);
-    if ((*(int32_t *)(rax + 0x4) & 0x60000) != 0x0) {
+    if ((*(int32_t *) (rax + 0x4) & 0x60000) != 0x0) {
         [NSException raise:@"CALayerInvalidTree" format:@"expecting model layer not copy: %@"];
     }
     rax = *(r13 + *_OBJC_IVAR_$_CALayer._attr + 0x8);
-    if ((*(int32_t *)(rax + 0x4) & 0x60000) != 0x0) {
+    if ((*(int32_t *) (rax + 0x4) & 0x60000) != 0x0) {
         [NSException raise:@"CALayerInvalidTree" format:@"expecting model layer not copy: %@"];
         rax = *(r13 + *_OBJC_IVAR_$_CALayer._attr + 0x8);
     }
@@ -3322,8 +3310,8 @@ static BOOL _CAObject_automaticallyNotifiesObserversForKey(int arg0, int arg1) {
     }
     r15 = CA::Transaction::ensure_compat();
     r12 = *(rbx + r12 + 0x8);
-    rax = *(int32_t *)(r15 + 0x20);
-    *(int32_t *)(r15 + 0x20) = rax + 0x1;
+    rax = *(int32_t *) (r15 + 0x20);
+    *(int32_t *) (r15 + 0x20) = rax + 0x1;
     if (rax == 0x0) {
         os_unfair_lock_lock(*(r15 + 0x18));
     }
@@ -3331,15 +3319,14 @@ static BOOL _CAObject_automaticallyNotifiesObserversForKey(int arg0, int arg1) {
 
     loc_fa642:
     rax = *(r14 + *_OBJC_IVAR_$_CALayer._attr + 0x8);
-    if ((*(int32_t *)(rax + 0x4) & 0x60000) != 0x0) {
+    if ((*(int32_t *) (rax + 0x4) & 0x60000) != 0x0) {
         [NSException raise:@"CALayerInvalidTree" format:@"expecting model layer not copy: %@"];
         rax = *(r14 + *_OBJC_IVAR_$_CALayer._attr + 0x8);
     }
     rdi = *(rax + 0x8);
     if (rdi != 0x0) {
         CA::Layer::remove_sublayer(rdi, r15);
-    }
-    else {
+    } else {
         CFRetain(r14);
         CA::Layer::remove_from_context();
     }
@@ -3352,7 +3339,7 @@ static BOOL _CAObject_automaticallyNotifiesObserversForKey(int arg0, int arg1) {
         rax = rcx;
         rcx = *(rax + 0x8);
     } while (rcx != 0x0);
-    var_2C = *(int32_t *)(rax + 0xfc);
+    var_2C = *(int32_t *) (rax + 0xfc);
     CA::Layer::update_removed_sublayer(*(r13 + *_OBJC_IVAR_$_CALayer._attr + 0x8), r15);
     CA::Transaction::release_object(r15);
     rcx = *(r12 + 0x18);
@@ -3373,15 +3360,15 @@ static BOOL _CAObject_automaticallyNotifiesObserversForKey(int arg0, int arg1) {
     if (rsi < rdi) goto loc_fa762;
 
     loc_fa78d:
-    if (*(int8_t *)(rcx + *_OBJC_IVAR_$_CALayerArray._ivars + 0x20) != 0x0) goto loc_fa81c;
+    if (*(int8_t *) (rcx + *_OBJC_IVAR_$_CALayerArray._ivars + 0x20) != 0x0) goto loc_fa81c;
 
     loc_fa798:
     *(rax + rbx * 0x8) = r14;
     CA::Layer::update_added_sublayer(*(r14 + *_OBJC_IVAR_$_CALayer._attr + 0x8), r15, r12);
     rdi = *(r14 + *_OBJC_IVAR_$_CALayer._attr + 0x8);
-    rax = sign_extend_64(*(int32_t *)(r15 + 0x8));
-    if ((rax < 0x0) || (*(int32_t *)(rdi + rax * 0x4 + 0x100) == 0x0)) {
-        *(int32_t *)CA::Layer::thread_flags_(rdi);
+    rax = sign_extend_64(*(int32_t *) (r15 + 0x8));
+    if ((rax < 0x0) || (*(int32_t *) (rdi + rax * 0x4 + 0x100) == 0x0)) {
+        *(int32_t *) CA::Layer::thread_flags_(rdi);
     }
     CA::Layer::update_for_changed_sublayers(r12, r15);
     CA::Layer::end_change(r12, r15, 0x1cd, @"sublayers");
